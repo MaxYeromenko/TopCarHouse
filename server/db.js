e46736b8918ad2b1b423083603f635ed967074f6
Максим Єрьоменко <eromenko.max@gmail.com>
2024-09-17 23:27:27 +0300

    a

---
+++ server/db.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const uri = process.env.MONGODB_URI;
+
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('Connected to MongoDB Atlas'))
+    .catch(err => console.error('Error connecting to MongoDB:', err));
+
+module.exports = mongoose;
