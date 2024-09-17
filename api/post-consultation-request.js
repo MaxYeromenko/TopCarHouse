0500ff9e0b00358470a211de787bda407839943f
Максим Єрьоменко <eromenko.max@gmail.com>
2024-09-17 23:14:32 +0300

    a

--- api/post-consultation-request.js
+++ api/post-consultation-request.js
@@ -1,31 +1,31 @@
 const mongoose = require('/api/post-consultation-request');
 
-const requestSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    phone: { type: String, required: true },
-    datetime: { type: Date, required: true }
-});
+// const requestSchema = new mongoose.Schema({
+//     name: { type: String, required: true },
+//     phone: { type: String, required: true },
+//     datetime: { type: Date, required: true }
+// });
 
-const ConsultationModel = mongoose.model('Consultation', requestSchema);
+// const ConsultationModel = mongoose.model('Consultation', requestSchema);
 
-module.exports = async (req, res) => {
-    if (req.method === 'POST') {
-        const { name, phone, datetime } = req.body;
+// module.exports = async (req, res) => {
+//     if (req.method === 'POST') {
+//         const { name, phone, datetime } = req.body;
 
-        try {
-            const newUser = new ConsultationModel({
-                name,
-                phone,
-                datetime
-            });
+//         try {
+//             const newUser = new ConsultationModel({
+//                 name,
+//                 phone,
+//                 datetime
+//             });
 
-            await newUser.save();
-            res.status(201).json({ success: true, message: 'Заява на консультацію успішно відправлена!' });
-        } catch (err) {
-            console.error(err);
-            res.status(500).json({ success: false, message: 'Помилка сервера під час реєстрації!' });
-        }
-    } else {
-        res.status(405).json({ success: false, message: 'Метод не дозволений!' });
-    }
-};
+//             await newUser.save();
+//             res.status(201).json({ success: true, message: 'Заява на консультацію успішно відправлена!' });
+//         } catch (err) {
+//             console.error(err);
+//             res.status(500).json({ success: false, message: 'Помилка сервера під час реєстрації!' });
+//         }
+//     } else {
+//         res.status(405).json({ success: false, message: 'Метод не дозволений!' });
+//     }
+// };
