115ed31bf18960fca851e481588c4162b214aae9
Максим Єрьоменко <eromenko.max@gmail.com>
2024-09-17 23:30:48 +0300

    a

--- api/get-product-by-id.js
+++ api/get-product-by-id.js
@@ -1,9 +1,4 @@
-const mongoose = require('mongoose');
-const uri = process.env.MONGODB_URI;
-
-mongoose.connect(uri)
-    .then(() => console.log('Connected to MongoDB Atlas'))
-    .catch(err => console.error('Error connecting to MongoDB Atlas:', err));
+const mongoose = require('../server/db');
 
 const DataSchema = new mongoose.Schema({
     brand: String,
