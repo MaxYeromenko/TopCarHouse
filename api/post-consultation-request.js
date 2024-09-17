e88ebc6e12d4817bcf4daee0a9e54f237930f954
Максим Єрьоменко <eromenko.max@gmail.com>
2024-09-17 23:04:07 +0300

    a

--- api/post-consultation-request.js
+++ api/post-consultation-request.js
@@ -1,11 +1,3 @@
-const mongoose = require('mongoose');
-
-const uri = process.env.MONGODB_URI;
-
-mongoose.connect(uri)
-    .then(() => console.log('Connected to MongoDB Atlas'))
-    .catch(err => console.error('Error connecting to MongoDB:', err));
-
 const requestSchema = new mongoose.Schema({
     name: { type: String, required: true },
     phone: { type: String, required: true },
