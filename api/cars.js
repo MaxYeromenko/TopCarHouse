58c605e712948ff4973941b2cdf97970d3980878
Максим Єрьоменко <eromenko.max@gmail.com>
2024-09-17 23:20:17 +0300

    a

--- api/cars.js
+++ api/cars.js
@@ -1,3 +1,9 @@
+const mongoose = require('mongoose');
+const uri = process.env.MONGODB_URI;
+
+mongoose.connect(uri)
+    .then(() => console.log('Connected to MongoDB Atlas'))
+    .catch(err => console.error('Error connecting to MongoDB Atlas:', err));
 
 const DataSchema = new mongoose.Schema({
     brand: String,
