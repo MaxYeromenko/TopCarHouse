691aa28e0f20563d65b4d8de93fdb95f0cf26c21
Максим Єрьоменко <eromenko.max@gmail.com>
2024-09-22 11:26:07 +0300

    a

--- api/get-user.js
+++ api/get-user.js
@@ -26,7 +26,7 @@ module.exports = async (req, res) => {
                 return res.status(400).json({ success: false, message: 'Неправильний email або пароль' });
             }
 
-            const token = jwt.sign({ id: user._id, name: user.name }, secret, { expiresIn: '1h' });
+            const token = jwt.sign({ id: user._id, name: user.name }, secret, { expiresIn: '1m' });
 
             res.status(200).json({ success: true, token, message: 'Вхід успішний!' });
         } catch (err) {
