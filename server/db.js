1aac6567b8cb37dc0b76a0f971a3ba635886091c
Максим Єрьоменко <eromenko.max@gmail.com>
2024-09-23 23:43:13 +0300

    a

--- server/db.js
+++ server/db.js
@@ -1,9 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI, {
-    maxPoolSize: 10,
-    socketTimeoutMS: 45000,
-    serverSelectionTimeoutMS: 5000
-}).catch(err => console.error('Error connecting to MongoDB:', err));
+mongoose.connect(process.env.MONGODB_URI).catch(err => console.error('Error connecting to MongoDB:', err));
 
 module.exports = mongoose;
