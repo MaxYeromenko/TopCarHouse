ee27c4bba3e3a67ce9adbd401c308274069fa9d1
Максим Єрьоменко <eromenko.max@gmail.com>
2024-09-24 00:01:49 +0300

    a

--- server/db.js
+++ server/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const uri = process.env.MONGODB_URI || 'mongodb+srv://user:TtGHeh6JZfcCkSNE@topcarhouse.f44si.mongodb.net/topcarhousedb?retryWrites=true&w=majority&appName=TopCarHouse';
+const uri = 'mongodb+srv://user:TtGHeh6JZfcCkSNE@topcarhouse.f44si.mongodb.net/topcarhousedb?retryWrites=true&w=majority&appName=TopCarHouse';
 
 mongoose.connect(uri, {
     maxPoolSize: 500,
