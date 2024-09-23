a6e6d8a8890583a202a2bef8f737e40b949db9eb
Максим Єрьоменко <eromenko.max@gmail.com>
2024-09-23 23:30:12 +0300

    a

--- server/db.js
+++ server/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI, {
+mongoose.connect('mongodb://root:pcvaP59lVECH028k@topcarhouse-shard-00-00.f44si.mongodb.net:27017,topcarhouse-shard-00-01.f44si.mongodb.net:27017,topcarhouse-shard-00-02.f44si.mongodb.net:27017/topcarhousedb?retryWrites=true&w=majority', {
     maxPoolSize: 10,
     socketTimeoutMS: 45000,
     serverSelectionTimeoutMS: 5000
