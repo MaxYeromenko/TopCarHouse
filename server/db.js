2dc0d8501756fb87624a9b199f305d048016146b
Максим Єрьоменко <eromenko.max@gmail.com>
2024-09-24 08:59:26 +0300

    a

--- server/db.js
+++ server/db.js
@@ -1,13 +1,15 @@
 const mongoose = require('mongoose');
-
-// const uri = 'mongodb+srv://root:pcvaP59lVECH028k@topcarhouse.f44si.mongodb.net/topcarhousedb?retryWrites=true&w=majority&appName=TopCarHouse';
-const uri = `mongodb://root:pcvaP59lVECH028k@topcarhouse-shard-00-00.puton.mongodb.net:27017,topcarhouse-shard-00-01.puton.mongodb.net:27017,topcarhouse-shard-00-02.puton.mongodb.net:27017/topcarhousedb`;
+const uri = `mongodb://root:pcvaP59lVECH028k@topcarhouse-shard-00-00.f44si.mongodb.net:27017,topcarhouse-shard-00-01.f44si.mongodb.net:27017,topcarhouse-shard-00-02.f44si.mongodb.net:27017/topcarhousedb?ssl=true&replicaSet=atlas-xyz-shard-0&authSource=admin&retryWrites=true&w=majority`;
 
 try {
-    mongoose.connect(uri);
+    mongoose.connect(uri, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    });
     console.log("Connected to MongoDB Atlas!");
 } catch (err) {
     console.error("Error connecting to MongoDB Atlas:", err);
 }
 
 module.exports = mongoose;
+
