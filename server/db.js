3c98ab1dd98206556c8a40b658f983e66205ac81
Максим Єрьоменко <eromenko.max@gmail.com>
2024-09-24 10:25:27 +0300

    a

--- server/db.js
+++ server/db.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose');
 
 const uri = 'mongodb+srv://root:pcvaP59lVECH028k@topcarhouse.f44si.mongodb.net/topcarhousedb?retryWrites=true&w=majority&appName=TopCarHouse';
 
-const connectDB = async () => {
+const connectDB = () => {
     try {
-        await mongoose.connect(uri);
+        mongoose.connect(uri);
         console.log("Connected to MongoDB Atlas!");
     } catch (err) {
         console.error("Error connecting to MongoDB Atlas:", err);
