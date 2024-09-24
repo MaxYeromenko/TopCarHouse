7fb30721c159a1200520f375e69c73709ac05213
Максим Єрьоменко <eromenko.max@gmail.com>
2024-09-24 08:53:55 +0300

    a

--- server/db.js
+++ server/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 // const uri = 'mongodb+srv://root:pcvaP59lVECH028k@topcarhouse.f44si.mongodb.net/topcarhousedb?retryWrites=true&w=majority&appName=TopCarHouse';
-const uri = `mongodb://root:pcvaP59lVECH028k@topcarhouse-shard-00-00.f44si.mongodb.net:27017,topcarhouse-shard-00-01.f44si.mongodb.net:27017,topcarhouse-shard-00-02.f44si.mongodb.net:27017/topcarhousedb`;
+const uri = `mongodb://root:pcvaP59lVECH028k@topcarhouse-shard-00-00.puton.mongodb.net:27017,topcarhouse-shard-00-01.puton.mongodb.net:27017,topcarhouse-shard-00-02.puton.mongodb.net:27017/topcarhousedb`;
 
 try {
     mongoose.connect(uri);
