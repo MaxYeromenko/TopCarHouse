d890b8e1f2d5f111acb72447c5e018c17b6e566f
Максим Єрьоменко <eromenko.max@gmail.com>
2024-09-28 00:57:03 +0300

    a

--- src/js/other/index.html/log-forms-switching.js
+++ src/js/other/index.html/log-forms-switching.js
@@ -7,6 +7,7 @@ const authContainer = document.getElementById('auth-container');
 const toggleButtonsVisibility = (showLogin) => {
     toggleElementsVisibility(logButtons, showLogin ? 'inline' : 'none');
     toggleElementsVisibility(logOutButtons, showLogin ? 'none' : 'inline');
+    toggleElementsVisibility(modalWindow, 'none')
 };
 
 if (isAuthTokenExpired()) {
