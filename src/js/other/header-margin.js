d90bdc3f10b0fca4e4f083f876e98551802fb467
Максим Єрьоменко <eromenko.max@gmail.com>
2024-09-30 14:03:11 +0300

    a

--- src/js/other/header-margin.js
+++ src/js/other/header-margin.js
@@ -1,3 +1,18 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const head = document.querySelector('head');
+
+    const existingIcons = head.querySelectorAll('link[rel="icon"], link[rel="shortcut icon"]');
+    existingIcons.forEach(icon => head.removeChild(icon));
+
+    const pageIcon = document.createElement('link');
+    pageIcon.setAttribute('rel', 'icon');
+    pageIcon.setAttribute('type', 'image/png');
+    pageIcon.setAttribute('sizes', '256x256');
+    pageIcon.setAttribute('href', 'https://res.cloudinary.com/dukwtlvte/image/upload/v1727693527/topcarhouse-favicon-color-256x256_mf8lcb.png');
+
+    head.appendChild(pageIcon);
+});
+
 let headerBlock = document.querySelector('header');
 let mainBlock = document.querySelector('main');
 
