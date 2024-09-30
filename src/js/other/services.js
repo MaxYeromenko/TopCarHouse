756085aa4aa57baad4d9f18101b0e18198c349c4
Максим Єрьоменко <eromenko.max@gmail.com>
2024-10-01 00:49:52 +0300

    a

---
+++ src/js/other/services.js
@@ -0,0 +1,9 @@
+const servicesButtons = document.querySelectorAll('.services-button');
+const servicesContainer = document.getElementById('services-container');
+
+servicesButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        toggleElementVisibility(modalWindow, 'flex');
+        toggleElementVisibility(servicesContainer, 'grid');
+    });
+});
