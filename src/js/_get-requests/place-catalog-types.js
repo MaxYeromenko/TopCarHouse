6c50c2e74341f533b44c2f4a7fb04cf478a11bb8
Максим Єрьоменко <eromenko.max@gmail.com>
2024-10-07 11:30:15 +0300

    a

--- src/js/_get-requests/place-catalog-types.js
+++ src/js/_get-requests/place-catalog-types.js
@@ -48,6 +48,7 @@ function placeCatalogTypes(catalogTypes) {
 
     for (const brand of catalogTypes.brands.sort()) {
         const brandLink = document.createElement('a');
+        brandLink.href = '#';
         brandLink.textContent = brand;
         brandLink.addEventListener('click', (e) => {
             e.preventDefault()
@@ -84,14 +85,89 @@ function displayCars(cars) {
     carsContainer.innerHTML = '';
 
     cars.forEach((car) => {
-        const carElement = document.createElement('div');
-        carElement.classList.add('car-card');
-        carElement.innerHTML = `
-            <h3>${car.brand} ${car.model}</h3>
-            <p>Year: ${car.year}</p>
-            <p>Price: ${car.price}</p>
-            <img src="${car.images[0] || '/default-car-image.jpg'}" alt="${car.model}">
-        `;
-        carsContainer.appendChild(carElement);
+        carsContainer.appendChild(createCarCard(car));
     });
-}
+};
+
+function createCarCard(car) {
+    const carCard = document.createElement('div');
+    carCard.classList.add('car');
+
+    let carImages = car.images && car.images.length > 0
+        ? car.images.map(image => image.startsWith('http') ? image : `${cloudinaryURL}${image}`)
+        : [default_car_URL];
+
+    let currentImageIndex = 0;
+    let validImages = [];
+    let imageInterval;
+
+    const carImageElement = document.createElement('img');
+    carImageElement.classList.add('product-image');
+    carImageElement.alt = `${car.brand} ${car.model}`;
+    carImageElement.src = default_car_URL;
+
+    const imageContainer = document.createElement('div');
+    imageContainer.classList.add('product-image-container');
+    imageContainer.appendChild(carImageElement);
+
+    const compareButton = document.createElement('button');
+    compareButton.setAttribute('data-id', car._id);
+    compareButton.classList.add('compare-btn');
+    compareButton.innerHTML = 'Порівняти <i class="fa-solid fa-scale-unbalanced"></i>';
+    imageContainer.appendChild(compareButton);
+
+    compareButton.addEventListener('click', function () {
+        const carId = this.getAttribute('data-id');
+        addToCarToLocalStorage(carId);
+    });
+
+    carCard.innerHTML = `
+        <div class="product-card">
+            <div class="product-info">
+                <h2 class="product-title">${car.brand} ${car.model}</h2>
+                <p class="product-description">
+                    ${car.year}, ${car.features.engine}, ${car.features.fuel_type}, ${car.features.horsepower} к.с.
+                </p>
+                <div class="product-features">
+                    <div class="feature"><i class="fa-solid fa-gears"></i>${car.features.transmission}</div>
+                    <div class="feature"><i class="fa-solid fa-gas-pump"></i>${car.features.fuel_consumption > 0 ? car.features.fuel_consumption + ' л / 100 км' : ' - '}</div>
+                    <div class="feature"><i class="fa-solid fa-caravan"></i>${car.features.horsepower} к.с.</div>
+                </div>
+                <div class="product-price">Ціна: $${car.price}</div>
+                <a href="/pages/product-info.html?id=${car._id}" target="_blank">Детальніше <i class="fa-solid fa-arrow-up-right-from-square"></i></a>
+            </div>
+        </div>`;
+
+    carImages.forEach(imageUrl => {
+        checkImageValidity(imageUrl)
+            .then(validImageUrl => {
+                validImages.push(validImageUrl);
+                if (validImages.length === 1) {
+                    carImageElement.src = validImages[0];
+                }
+            })
+            .catch(() => console.warn(`Image ${imageUrl} is broken and will be removed.`));
+    });
+
+    carCard.querySelector('.product-card').insertBefore(imageContainer, carCard.querySelector('.product-info'));
+
+    const changeImage = () => {
+        currentImageIndex = (currentImageIndex + 1) % validImages.length;
+        carImageElement.src = validImages[currentImageIndex];
+    };
+
+    carCard.addEventListener('mouseenter', () => {
+        if (validImages.length > 1) {
+            imageInterval = setInterval(changeImage, 1500);
+        }
+    });
+
+    carCard.addEventListener('mouseleave', () => {
+        clearInterval(imageInterval);
+        if (validImages.length > 0) {
+            carImageElement.src = validImages[0];
+        }
+    });
+
+    return carCard;
+};
