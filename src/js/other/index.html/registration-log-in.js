ba7f1316a63b1ff79e6404b7b2cac49f8d686cd9
Максим Єрьоменко <eromenko.max@gmail.com>
2024-10-22 19:20:01 +0300

    a

--- src/js/other/index.html/registration-log-in.js
+++ src/js/other/index.html/registration-log-in.js
@@ -43,3 +43,25 @@ form.addEventListener('submit', async (e) => {
         showMessage('Помилка сервера, будь ласка, відправте дані ще раз або перезавантажте сторінку!', false);
     }
 });
+
+
+document.querySelector('#login-box form').addEventListener('submit', async (event) => {
+    event.preventDefault();
+
+    const email = event.target.querySelector('input[type="email"]').value;
+    const password = event.target.querySelector('input[type="password"]').value;
+    showMessage('Завантаження...', true);
+
+    try {
+        const result = await fetchWithRetryPost('/api/get-user', { email, password }, retriesLimit);
+
+        if (result.success) {
+            localStorage.setItem('jwtToken', result.token);
+            toggleButtonsVisibility(false);
+            showMessage(result.message, true);
+        }
+    } catch (error) {
+        console.error('Error fetching data:', error);
+        showMessage('Помилка сервера, будь ласка, відправте дані ще раз або перезавантажте сторінку!', false);
+    }
+});
