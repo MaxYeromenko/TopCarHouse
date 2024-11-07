116176a60e3f5486aa917e3aac7323170fd2ccca
Максим Єрьоменко <eromenko.max@gmail.com>
2024-11-07 15:36:03 +0200

    a

--- src/js/other/reset-password.html/script.js
+++ src/js/other/reset-password.html/script.js
@@ -1,11 +1,25 @@
+import {
+    createConsultationRequest,
+    themeApplication,
+    showServicesModalWindow
+} from '../_utils/script.js';
+
+themeApplication();
+createConsultationRequest();
+showServicesModalWindow();
+
+const urlParams = new URLSearchParams(window.location.search);
+const email = urlParams.get('email');
+const token = urlParams.get('token');
+
 const resetPasswordBox = document.getElementById('reset-password-box');
 const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
+
+resetPasswordBox.querySelector('p').textContent = email;
 
 resetPasswordBox.querySelector('form').addEventListener('submit', async (event) => {
     event.preventDefault();
 
-    const urlParams = new URLSearchParams(window.location.search);
-    const token = urlParams.get('token');
     const newPassword = String(event.target.querySelector('input[name="new-password"]').value).trim();
     const confirmPassword = String(event.target.querySelector('input[name="confirm-password"]').value).trim();
 
@@ -19,7 +33,7 @@ resetPasswordBox.querySelector('form').addEventListener('submit', async (event) 
         return;
     }
 
-    showMessage('Скидання пароля...', true);
+    showMessage('Пароль змінюється...', true);
 
     try {
         const result = await fetchWithRetryPost('/api/reset-password', { token, newPassword }, retriesLimit);
@@ -27,7 +41,9 @@ resetPasswordBox.querySelector('form').addEventListener('submit', async (event) 
         if (result.success) {
             event.target.reset();
             showMessage('Пароль успішно змінений! Тепер ви можете увійти.', true);
-            window.location.href = '/';
+            setTimeout(() => {
+                window.location.href = '/';
+            }, 3000);
         } else {
             showMessage(result.message, false);
         }
