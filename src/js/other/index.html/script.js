a4a01684374c8767953db120d57070b0c99ccfbb
Максим Єрьоменко <eromenko.max@gmail.com>
2024-11-20 15:36:45 +0200

    a

--- src/js/other/index.html/script.js
+++ src/js/other/index.html/script.js
@@ -205,6 +205,8 @@ function initializeFormSubmission() {
         const images = formData.getAll('images');
         const imageUrls = [];
 
+        console.log(images);
+
         if (images.length > 5) {
             showMessage('Можна завантажити не більше 5 зображень!', false);
             return;
